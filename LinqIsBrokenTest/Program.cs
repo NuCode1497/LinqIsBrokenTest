2649341fdc9cc62f041c91426e1acf548e5563e9
Cody <cody@smartxadvisory.com>
2019-02-05 14:17:26 -0500

    Add project files.

---
+++ LinqIsBrokenTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqIsBrokenTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
